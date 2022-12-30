708ccccd6e59ec2d60f597dafb8979f74a8eb2b0
devprabhu18 <107057399+devprabhu18@users.noreply.github.com>
2022-12-30 18:42:55 +0530

    Add files via upload

---
+++ day-08/day8dev.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>                                             // Math library for sqrt() etc.
+#include<cstdio>                                            //for printf and scanf in c++
+using namespace std;
+int isPrime(int num){                                       //Function to check Prime number
+    if(num<2){
+        return 0;
+    }
+    for(int i=2;i<=sqrt(num);i++){
+        if(num%i == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+int main(){
+    int m,n;
+    cout<<"Enter the numbers"<<endl;
+    cin>>m>>n;
+    int prev=-1;
+    for(int i=m;i<=n;i++){                         
+        if(isPrime(i)) {                                    //Calling the function
+            if(prev != -1){
+                printf("%d - %d : %d\n",prev,i,i-prev-1);
+            }
+            prev=i;
+        }
+    }
+    return 0;
+}
